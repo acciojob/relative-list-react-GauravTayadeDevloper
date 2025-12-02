0bb10841935e2bed01e5c634209e32ed9a2a9203
GauravTayadeDevloper <gauravtayade619@gmail.com>
2025-12-02 18:43:06 +0530

    Your commit message

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const App = () => {
+let relatives =["kaka","mama","kaku","mami","nani","nana"]
+
   return (
     <div id="main">
                {/* Do not remove the main div */}
+      <ol>
+        {relatives.map((name,index)=>(
+          <li key={"relativeList"+index.toString()}>
+            {name}
+        </li>))}
+      </ol>
     </div>
   )
 }
